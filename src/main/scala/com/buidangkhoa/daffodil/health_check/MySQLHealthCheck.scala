5b804cf12b818c4dd4f7afb202301c34c319bce9
Khoa Bui <kbui@zendesk.com>
2020-11-16 02:32:47 +0000

    Add error logging

--- src/main/scala/com/buidangkhoa/daffodil/health_check/MySQLHealthCheck.scala
+++ src/main/scala/com/buidangkhoa/daffodil/health_check/MySQLHealthCheck.scala
@@ -3,16 +3,22 @@ package com.buidangkhoa.daffodil.health_check
 import cats.effect.IO
 import doobie.util.transactor.Transactor
 import doobie.implicits._
+import com.typesafe.scalalogging.LazyLogging
 
-object MySQLHealthCheck {
+object MySQLHealthCheck extends LazyLogging {
   def check(transactor: Transactor[IO]): IO[HealthCheckResult] = {
     sql"""SELECT 1 + 1""".query[Int].unique
       .transact(transactor)
       .map(_ => HealthCheckResult(name = "MySQL", status = true, "Connected"))
-      .handleErrorWith(ex => IO.pure(HealthCheckResult(
-        name = "MySQL",
-        status = false,
-        s"Unable to connect. Reason: ${ex.getMessage}")
-      ))
+      .handleErrorWith(ex => {
+        for {
+          _ <- IO(logger.error(s"Failed to connect: ${ex.getMessage}"))
+          resp = HealthCheckResult(
+            name = "MySQL",
+            status = false,
+            "Unable to connect"
+          )
+        } yield resp
+      })
   }
 }
