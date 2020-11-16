e1409e923a59630cf3dc6aad41dde997fb0b4cb5
Khoa Bui <kbui@zendesk.com>
2020-11-16 01:24:28 +0000

    Simplify json encoder for HealthCheck

--- src/main/scala/com/buidangkhoa/daffodil/health_check/HealthCheckResult.scala
+++ src/main/scala/com/buidangkhoa/daffodil/health_check/HealthCheckResult.scala
@@ -1,9 +1,10 @@
 package com.buidangkhoa.daffodil.health_check
 
 import cats.Applicative
-import io.circe.{Encoder, Json}
+import io.circe.Encoder
 import org.http4s.EntityEncoder
 import org.http4s.circe.jsonEncoderOf
+import io.circe.generic.semiauto._
 
 case class HealthCheckResult(
                             name: String,
@@ -11,16 +12,8 @@ case class HealthCheckResult(
                             message: String
                             )
 object HealthCheckResult {
-  implicit val greetingEncoder: Encoder[HealthCheckResult] = new Encoder[HealthCheckResult] {
-    final def apply(a: HealthCheckResult): Json = {
-      Json.obj(
-        ("name", Json.fromString(a.name)),
-        ("status", Json.fromBoolean(a.status)),
-        ("message", Json.fromString(a.message))
-      )
-    }
-  }
-  implicit def healthCheckEncoder[F[_]: Applicative]: EntityEncoder[F, HealthCheckResult] = {
+  implicit def healthCheckEncoder: Encoder[HealthCheckResult] = deriveEncoder[HealthCheckResult]
+  implicit def healthCheckEntityEncoder[F[_]: Applicative]: EntityEncoder[F, HealthCheckResult] = {
     jsonEncoderOf[F, HealthCheckResult]
   }
 }
